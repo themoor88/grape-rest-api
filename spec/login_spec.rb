9119b888b1bc2ef973c022f63129cd2e4cb5d26e
WajidKagzi <wjkagzi@gmail.com>
2016-05-08 22:32:44 -0400

    Modifying test to make it pass and adding let statements

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 describe '/api/login' do
+  let(:email) { user.email }
+  let(:password) { user.password }
+  let!(:user) { create :user }
+  let(:original_params) { { email: email, password: password } }
+  let(:params) { original_params }
+  let(:params) { original_params.except(:password) }
+  it_behaves_like '400'
+  it_behaves_like 'json result'
+  it_behaves_like 'contains error msg', 'password is missing'
+
   context 'negative tests' do
     context 'missing params' do
       context 'password' do
